e3216d8e233fef0f9e1c4510de67499755f40831
ln1778 <465410291@qq.com>
2023-05-09 19:01:13 +0800

    dd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const axios_hooks=require("./axios_hooks").default;
+const useAxios=require("./axios_hooks").default;
 const config=require("./config").default;
 const commont=require("./commont").default;
  
@@ -7,6 +7,6 @@ const commont=require("./commont").default;
 const Index={useAxios,config,commont};
 export {commont};
 export {config};
-export {axios_hooks};
+export {useAxios};
 
 module.exports.default=Index;
